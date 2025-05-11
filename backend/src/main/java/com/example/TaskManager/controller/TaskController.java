2a323f45a159242a4fa82e24fd29e20ce7fd197d
P Shashi Kanth Reddy <shashikanth8938@gmail.com>
2025-05-11 12:13:39 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/TaskManager/controller/TaskController.java
@@ -0,0 +1,40 @@
+package com.example.TaskManager.controller;
+import com.example.TaskManager.model.Task;
+import com.example.TaskManager.repository.TaskRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "*")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
